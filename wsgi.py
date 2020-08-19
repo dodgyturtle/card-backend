b07c298b9615d2f864bc0b7776ca4ac47640094d
Aleksandr Semenov <semenlinux@gmail.com>
2020-08-19 06:52:33 +0000

    Setting Gunicorn

--- wsgi.py
+++ wsgi.py
@@ -2,4 +2,5 @@ from api_app import create_app
 
 application = create_app()
 
-#application.run(host="0.0.0.0", debug=True)
+if __name__ == "__main__":
+    application.run(host="0.0.0.0", debug=True)
