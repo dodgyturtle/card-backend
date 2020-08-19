89ac5bb3f44ff4e349fce70b4ef1eb487815c65a
Aleksandr Semenov <semenlinux@gmail.com>
2020-08-19 07:04:36 +0000

    Change tests for Heroku

--- tests/16_add_user_0_small.sh
+++ tests/16_add_user_0_small.sh
@@ -1,1 +1,1 @@
-curl -i -X POST "http://127.0.0.1:5000/v1/user" -H  "accept: application/json" -H  "Content-Type: application/json" -d "{  \"accountid\": \"sdsdfsf\"}"
+curl -i -X POST "https://card-backend-sas.herokuapp.com/v1/user" -H  "accept: application/json" -H  "Content-Type: application/json" -d "{  \"accountid\": \"dfgdfgdg\"}"
