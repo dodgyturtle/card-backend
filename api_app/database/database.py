97a3f72ba666d4f566eefb0c13cb017fd9924d25
Aleksandr Semenov <semenlinux@gmail.com>
2020-09-04 12:59:25 +0000

    Fix check ObjectiId in update/delete card

--- api_app/database/database.py
+++ api_app/database/database.py
@@ -1,4 +1,5 @@
 from api_app.database.model import User, Users_Cards
+from bson import ObjectId
 
 def get_user_info(accountid: str):
     user_info = User.objects.filter(accountid=accountid).first()
@@ -6,5 +7,7 @@ def get_user_info(accountid: str):
 
 
 def get_card_info(cardid: str):
+    if not ObjectId.is_valid(cardid):
+        return None
     card_info = Users_Cards.objects.filter(id=cardid).first()
     return card_info
